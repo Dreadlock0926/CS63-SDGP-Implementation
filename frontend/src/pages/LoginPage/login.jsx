3e9cfd0c23bce7d4dd0fa730794dcc0875ee4747
Haseeb Althaf <haseebalthaf1@outlook.com>
2024-02-04 17:24:57 +0530

    Added additional stuff to the login page.

--- frontend/src/pages/LoginPage/login.jsx
+++ frontend/src/pages/LoginPage/login.jsx
@@ -1,5 +1,6 @@
 import NavBar from "../../components/NavigationBar/navBar.jsx";
 import "../main.css";
+import "./login.css"
 
 
 function Login() {
@@ -8,6 +9,15 @@ function Login() {
         <NavBar />
         <div className="backgroundContainer">
             <img alt="background" className="bgImg2" src="./images/background2.png"></img>
+            <div className="loginContainer">
+                <img alt="avatar" className="avItem2" src="./images/avatar.png"></img> 
+                <p className="loginTitle">Login</p>
+                <p className="loginText">Dont have an account?<a href="/">Sign up</a></p>
+                <form className="loginForm">
+                    <input type="email" placeholder="Email" />
+                    <input type="password" placeholder="Password" />
+                </form>
+            </div>
         </div>
         </>
     );   
