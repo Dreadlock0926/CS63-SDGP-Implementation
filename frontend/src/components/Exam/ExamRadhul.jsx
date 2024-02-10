07e6b7f64322c338a5a7c5f8c4254c95a6acf8c6
Radhul Silva <radhulsilva@gmail.com>
2024-02-10 20:59:54 +0530

    Update ExamRadhul.jsx

--- frontend/src/components/Exam/ExamRadhul.jsx
+++ frontend/src/components/Exam/ExamRadhul.jsx
@@ -2,20 +2,21 @@ import React, { useState, useEffect } from "react";
 import "./ExamRadhul.css";
 
 function ExamTimer() {
-  const [isTimerRunning, setIsTimerRunning] = useState(false);
-  const [timeLeft, setTimeLeft] = useState(60); // Initial time in seconds
+  const [isTimerRunning, setIsTimerRunning] = useState(true);
+  const [timeLeft, setTimeLeft] = useState(10); // Initial time in seconds
 
   useEffect(() => {
-    let intervalId;
-
     if (isTimerRunning) {
+      let intervalId;
       intervalId = setInterval(() => {
-        setTimeLeft((prevTimeLeft) => Math.max(0, prevTimeLeft - 1)); // Decrement time, ensuring non-negative
+        if (timeLeft == 0) {
+          clearInterval(intervalId);
+          setIsTimerRunning(false);
+        }
+        setTimeLeft((timeLeft) => timeLeft - 1);
       }, 1000);
+      return () => clearInterval(intervalId);
     }
-
-    // Cleanup function to clear the interval on unmount or when `isTimerRunning` changes
-    return () => clearInterval(intervalId);
   }, [isTimerRunning]);
 
   const minutes = Math.floor(timeLeft / 60);
@@ -24,15 +25,8 @@ function ExamTimer() {
     .toString()
     .padStart(2, "0")}`;
 
-  const handleStartStop = () => {
-    setIsTimerRunning(!isTimerRunning);
-  };
-
   return (
     <div>
-      <button onClick={handleStartStop}>
-        {isTimerRunning ? "Stop Timer" : "Start Timer"}
-      </button>
       <div id="timerDisplay">{display}</div>
     </div>
   );
