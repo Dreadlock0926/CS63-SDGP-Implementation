ed768033c38fb499052f875799261a212863c35d
Dreadlock0926 <105129406+Dreadlock0926@users.noreply.github.com>
2024-02-15 00:01:12 +0530

    Question Display Fixed

--- frontend/src/components/QuestionComponent/QuestionComponent.jsx
+++ frontend/src/components/QuestionComponent/QuestionComponent.jsx
@@ -1,40 +1,65 @@
 import "./QuestionComponent.css"
 
-function QuestionComponent() {
+function SubQuestion( {sqNum, sqText} ) {
+    return (
+        <div className="sub-question">
+        <div className="sub-question-text-container">
+            <div className="sq-num">{sqNum}</div>
+            <div className="sub-question-text">{sqText}</div>
+        </div>
+        <div className="figure-for-sub-question"></div>
+        <div className="answer-for-sub-question">
+            <input className="answer-input" placeholder="Answer..."></input>
+            <div className="mark-for-sq">(2 marks)</div>
+        </div>
+    </div>
+    )
+}
+
+function QuestionComponent( {question, mqNum} ) {
 
     return (
         <>
         <div className="question-component-container">
-            <div className="main-text-container">
-                <div className="mq-num">1</div>
-                <div className="main-text">The lengths of the rods produced by a company are normally distributed with mean 55.6 mm
-                and standard deviation 1.2 mm.</div>
+            <div className="main-question-container">
+                <div className="main-text-container">
+                    <div className="mq-num">1</div>
+                    <div className="main-text">The lengths of the rods produced by a company are normally distributed with mean 55.6 mm
+                    and standard deviation 1.2 mm.</div>
+                </div>
+                {/* <input className="answer-input" placeholder="Answer..."></input> */}
             </div>
             <div className="sub-question-container">
-                <div className="sub-question">
-                    <div className="sub-question-text-container">
-                        <div className="sq-num">a</div>
-                        <div className="sub-question-text">In a random sample of 400 of these rods, how many would you expect to have
-                        length less than 54.8mm?</div>
-                    </div>
-                    <div className="figure-for-sub-question"></div>
-                    <div className="answer-for-sub-question">
-                        <input className="answer-input" placeholder="Answer..."></input>
-                        <div className="mark-for-sq">(2 marks)</div>
-                    </div>
+                    <SubQuestion sqNum={"a"} sqText={"In a random sample of 400 of these rods, how many would you expect to have length less than 54.8mm?"} />
+                    <SubQuestion sqNum={"b"} sqText={"Find the probability that a randomly chosen rod produced by this company has a length that is within half a standard deviation of the mean."} />
+            </div>
+        </div>
+        <div className="question-component-container">
+            <div className="main-question-container">
+                <div className="main-text-container">
+                    <div className="mq-num">1</div>
+                    <div className="main-text">The lengths of the rods produced by a company are normally distributed with mean 55.6 mm
+                    and standard deviation 1.2 mm.</div>
                 </div>
-                <div className="sub-question">
-                    <div className="sub-question-text-container">
-                        <div className="sq-num">b</div>
-                        <div className="sub-question-text">Find the probability that a randomly chosen rod produced by this company
-                        has a length that is within half a standard deviation of the mean.</div>
-                    </div>
-                    <div className="figure-for-sub-question"></div>
-                    <div className="answer-for-sub-question">
-                        <input className="answer-input" placeholder="Answer..."></input>
-                        <div className="mark-for-sq">(2 marks)</div>
-                    </div>
+                {/* <input className="answer-input" placeholder="Answer..."></input> */}
+            </div>
+            <div className="sub-question-container">
+                    <SubQuestion sqNum={"a"} sqText={"In a random sample of 400 of these rods, how many would you expect to have length less than 54.8mm?"} />
+                    <SubQuestion sqNum={"b"} sqText={"Find the probability that a randomly chosen rod produced by this company has a length that is within half a standard deviation of the mean."} />
+            </div>
+        </div>
+        <div className="question-component-container">
+            <div className="main-question-container">
+                <div className="main-text-container">
+                    <div className="mq-num">1</div>
+                    <div className="main-text">The lengths of the rods produced by a company are normally distributed with mean 55.6 mm
+                    and standard deviation 1.2 mm.</div>
                 </div>
+                {/* <input className="answer-input" placeholder="Answer..."></input> */}
+            </div>
+            <div className="sub-question-container">
+                    <SubQuestion sqNum={"a"} sqText={"In a random sample of 400 of these rods, how many would you expect to have length less than 54.8mm?"} />
+                    <SubQuestion sqNum={"b"} sqText={"Find the probability that a randomly chosen rod produced by this company has a length that is within half a standard deviation of the mean."} />
             </div>
         </div>
         </>
--- frontend/src/pages/TestPages/ExamQuestionTest.jsx
+++ frontend/src/pages/TestPages/ExamQuestionTest.jsx
@@ -2,8 +2,78 @@ import QuestionComponent from "../../components/QuestionComponent/QuestionCompon
 
 function ExamQuestionTest() {
 
+    const oneAnswerQuestion = {
+        questionID: "p1_s_2_w_2022_2",
+        questionTopic: "Series",
+        questionsGrid: [
+          "The first, second and third terms of an arithmetic progression are a, 2a and a^2 respectively, where a is a positive constant. Find the sum of the first 50 terms of the progression."
+        ],
+        questionsFiguresGrid: [
+          ""
+        ],
+        answersTypeGrid: [
+          "Expression"
+        ],
+        answersGrid: [
+          "3825"
+        ],
+        questionSource: "Pure Mathematics I"
+    }
+
+    const contextQuestion = {
+        questionID: "s1_p_5_w_2022_2",
+        questionTopic: "Probability",
+        questionsGrid: [
+            "Eric has three coins. One of the coins is fair. The other two coins are each biased so that the probability of obtaining a head on any throw is 1/4, independently of all other throws. Eric throws all three coins at the same time. Events A and B are defined as follows. A: all three coins show the same result B: at least one of the biased coins shows a head",
+            "Find P(B).",
+            " Find P(A|B)."
+        ],
+        questionsFiguresGrid: [
+            "",
+            "",
+            ""
+        ],
+        answersTypeGrid: [
+            "Expression",
+            "Expression",
+            "Expression"
+        ],
+        answersGrid: [
+            "",
+            "7/16",
+            "1/14"
+        ],
+        questionSource: "Probability and Statistics I"
+    }
+
+    const noContextTestQuestion = {
+        questionID: "s1_pac_7_w_2022_2",
+        questionTopic: "Permutations and combinations",
+        questionsGrid: [
+            "Find the number of different arrangements of the 9 letters in the word ALLIGATOR in which the two As are together and the two Ls are together.",
+            "The 9 letters in the word ALLIGATOR are arranged in a random order. Find the probability that the two Ls are together and there are exactly 6 letters between the two As. ",
+            "Find the number of different selections of 5 letters from the 9 letters in the word ALLIGATOR which contain at least one A and at most one L."
+        ],
+        questionsFiguresGrid: [
+            "",
+            "",
+            ""
+        ],
+        answersTypeGrid: [
+            "Expression",
+            "Expression",
+            "Expression"
+        ],
+        answersGrid: [
+            "5040",
+            "0.0132",
+            "35"
+        ],
+        questionSource: "Probability and Statistics I"
+    }
+
     return (
-        <QuestionComponent />
+        <QuestionComponent question={noContextTestQuestion} />
     )
 }
 
