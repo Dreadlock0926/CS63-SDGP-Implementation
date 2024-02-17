09775efa61326a642d69725962432f8761a26f28
Veloxal <dulransamarasinghe4@gmail.com>
2024-02-17 14:15:56 +0530

    rough sketch to trace progress overtime (progress tracking)

--- frontend/src/pages/TestPages/ExamFinal.jsx
+++ frontend/src/pages/TestPages/ExamFinal.jsx
@@ -1,22 +1,33 @@
 /* eslint-disable no-unused-vars */
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import { UserContext } from "../../App";
 
 const ExamFinalized = () => {
-  const [marks, setMarks] = useState(20);
+  // const {} = useContext(UserContext)
+  const [marks, setMarks] = useState(0);
+  const [tracker,setTracker] = useState(0)
 
   //might have to use some local storage approach for this!
 
-  useEffect(()=>{
-  
-    const item = localStorage.getItem("time");
-    item ? setMarks(item) : console.log('Invalid no time!');
+  useEffect( ()=>{
+      const oldProgress = localStorage.getItem("progress");
+    if(oldProgress){
+      setTracker((prev)=>prev+=oldProgress)
+    } else{
+      const item = localStorage.setItem("progress",marks);
+      item ? setMarks(item) : console.log('Invalid no time!');
+    }
+   
   },[])
 
+
+  
   return (
-    <div>
+    <div style={{textAlign:"center",color:"white"}}>
       <h1>Your Exam is Over!</h1>
       <p>Marks {marks}</p>
+      <p>{`Tracking records -> ${tracker}`}</p>
       <Link to="/">Go Back?</Link>
     </div>
   );
