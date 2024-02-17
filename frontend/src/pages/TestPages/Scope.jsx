9d0c4abbc779c5b1eb2a592d684ef75371570c9c
Radhul Silva <radhulsilva@gmail.com>
2024-02-17 21:14:19 +0530

    Scope For Exam
    implemented the scope feature for the exam

--- frontend/src/pages/TestPages/Scope.jsx
+++ frontend/src/pages/TestPages/Scope.jsx
@@ -1,132 +1,170 @@
 /* eslint-disable no-unused-vars */
-import { useContext, useEffect, useState } from "react";
-import { UserContext } from "../../App";
-import {useNavigation} from 'react-router-dom'
+import { useEffect, useReducer, useState } from "react";
 import Axios from "axios";
-// import Loading from "../Loading";
+import "./Scope.css";
 
 const Scope = () => {
-  // const navigation = useNavigation(); //bugged out
-  const { loading, setLoading } = useContext(UserContext);
-  const [data,setData] = useState([])
-  const [topics, setTopics] = useState({
-      topic1:false,
-      topic2:false,
-      topic3:false
-  });
-  const [toggle,setToggle] = useState(false);
+  const [selectAllTopics, setSelectAllTopics] = useState(false);
+  const [questions, setQuestions] = useState({});
 
-  const handleTopicChange = (topic) => {
-    setTopics((prevState) => ({
-      ...prevState,
-      [topic]: !prevState[topic],
-    }));
-  };
-
-
-  const selectScope = async (e) => {
-    e.preventDefault();
-    try {
-      setLoading(true);
-  
-          const selectedTopics = topics.filter((x)=>x===true) //filtering anything that's selected to be sent
-          const exam = await Axios.get("http://localhost:8000/exam/scope", { topics: selectedTopics }); //this path is not made yet!
-          if (exam.status === 200) {
-          setData(exam.data);
-          } else {
-            alert("Something went wrong!")
-          }
-      
-    } catch (err) {
-      console.error(err);
-      alert("Error occurred!");
-    } finally {
-      setLoading(false);
+  const reducer = (state, action) => {
+    switch (action.type) {
+      case "FETCH_MODULES":
+        return { ...state, modules: action.payload };
+      case "FETCH_TOPICS":
+        return { ...state, topics: action.payload };
+      case "SELECT_MODULE":
+        return { ...state, selectedModule: action.payload };
+      case "SELECT_TOPIC":
+        return { ...state, selectedTopic: action.payload };
+      default:
+        return state;
     }
   };
 
+  const initialState = {
+    modules: [],
+    topics: [],
+    selectedModule: "Pure Mathematics I",
+    selectedTopic: [],
+  };
 
-  const selectAllTopics = async ()=>{
-    // const checker = topics.forEach((x)=>x===false);  //verify this logic
-    
-      const exam = await Axios.get("http://localhost:8000/exam/scope" ); //this path is not made yet!
-      if (exam.status === 200) {
-        setData(exam.data);
-        localStorage.setItem("exams",data);
-        // navigator("/examination") //we must use this it's bugged out!
-        } else {
-          alert("Error while getting data back!");
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const getTopics = async (selectedModule) => {
+    try {
+      const response = await Axios.post(
+        "http://localhost:8000/addQuestion/getQuestionInfo",
+        {
+          source: selectedModule,
         }
-    
-  
-  }
+      );
+
+      dispatch({ type: "FETCH_TOPICS", payload: response.data.topics });
+    } catch (error) {}
+  };
+
+  const getModules = async () => {
+    try {
+      const response = await Axios.get(
+        "http://localhost:8000/addQuestion/getModules"
+      );
+
+      getTopics();
+      dispatch({ type: "FETCH_MODULES", payload: response.data });
+      dispatch({ type: "SELECT_MODULE", payload: response.data[0] });
+      getTopics(response.data[0]);
+    } catch (error) {}
+  };
 
   useEffect(() => {
-    console.log(`Selected topics: ${JSON.stringify(topics)}`);
-  }, [topics]);
+    getModules();
+  }, []);
 
-  function selectTopics(){
-    setToggle(!toggle)
-  }
+  const handleTopicChange = (event) => {
+    setSelectAllTopics(false);
 
-  // const selectionVal = document.querySelector(".selection");
-  // if(selectionVal.innerHTML==="pure"){
-  //   //show pure maths related topics
-  // }else if(selectionVal.innerHTML==="stat"){
-  //   //show stat related topics
-  // }else{
-  //   alert("Invalid option!") //no way this happens
-  // }
-  
+    const { checked } = event.target;
+    const newSelectedTopics = checked
+      ? [...state.selectedTopic, event.target.value]
+      : state.selectedTopic.filter((topic) => topic !== event.target.value);
+
+    dispatch({ type: "SELECT_TOPIC", payload: newSelectedTopics });
+  };
+
+  const handleModuleChange = (event) => {
+    setSelectAllTopics(false);
+    dispatch({ type: "SELECT_TOPIC", payload: [] });
+    dispatch({ type: "SELECT_MODULE", payload: event.target.value });
+    getTopics(event.target.value);
+  };
+
+  const retrieveQuestions = async () => {
+    if (state.selectedTopic.length === 0) {
+      alert("Please select at least one topic");
+      return;
+    } else {
+      try {
+        console.log("state.selectedTopic", state.selectedTopic);
+        const response = await Axios.post(
+          "http://localhost:8000/getQuestionsOnTopic/getQuestionsForExam",
+          {
+            topics: state.selectedTopic,
+          }
+        );
+
+        console.log(response.data);
+        setQuestions(response.data);
+      } catch (error) {}
+    }
+  };
 
   return (
-    <div style={{margin:"5%",padding:"5%"}}>
-                
-      {loading ? (
-        // <Loading />
-        "Loading..."
-      ) : (      
-        <div >   
-           <select className="selection"><option value={"pure"}>Pure Maths</option><option value={"stat"}>Statistics</option></select>
-           <br></br>
-           <button onClick={selectAllTopics} disabled={toggle}>Select From All</button> 
-          <p>{data&&data.length? JSON.stringify(data) : "No results found"}</p>   
-          <br></br>
-          <button onClick={selectTopics} className="action-button">{toggle?"Close Selector":`Select Specific Topics!`}</button>
-          <br></br>
-          {toggle?           
-          <form onSubmit={selectScope}>  
-        <span>
-          <h1>Topic 1</h1>
-          <input
-            type="checkbox"
-            name="topic1"
-            checked={topics.topic1}
-            onChange={() => handleTopicChange("topic1")}
-          />
-            <h1>Topic 2</h1>
-          <input
-            type="checkbox"
-            name="topic2"
-            checked={topics.topic2}
-            onChange={() => handleTopicChange("topic2")}
-          />
-           <h1>Topic 3</h1>
-          <input
-            type="checkbox"
-            name="topic3"
-            checked={topics.topic3}
-            onChange={() => handleTopicChange("topic3")}
-          />
-        </span>
-        {/* Add more topics similarly */}
-      <br></br>
-    
-        <button type="submit" disabled={loading}>Select Topics</button>
-     
-      </form>:<div><h1></h1></div>}
-</div>
-        
+    <div>
+      {state.modules.length > 0 ? ( // Check if modules are available
+        <>
+          <select
+            id="moduleDropdown"
+            onChange={handleModuleChange}
+            value={state.selectedModule}
+          >
+            {state.modules.map((module, i) => (
+              <option key={i} value={module}>
+                {module}
+              </option>
+            ))}
+          </select>
+
+          <button
+            onClick={() => {
+              if (!selectAllTopics) {
+                dispatch({ type: "SELECT_TOPIC", payload: state.topics });
+                setSelectAllTopics(true);
+              } else {
+                dispatch({ type: "SELECT_TOPIC", payload: [] });
+                setSelectAllTopics(false);
+              }
+            }}
+          >
+            Select All
+          </button>
+
+          <ul className="topicsCheckbox">
+            {state.topics.map((topic, i) => (
+              <li key={i}>
+                <input
+                  type="checkbox"
+                  name="topic"
+                  value={topic}
+                  checked={
+                    false || state.selectedTopic.includes(topic) ? true : false
+                  }
+                  onChange={handleTopicChange}
+                />
+                {topic}
+              </li>
+            ))}
+          </ul>
+          <p>Selected Topic: {state.selectedTopic.join(", ")}</p>
+          <p>Selected Module: {state.selectedModule}</p>
+          <button onClick={retrieveQuestions}>Get Questions</button>
+
+          {questions && questions.length > 0 ? (
+            <ul>
+              {questions.map((question, i) => (
+                <li key={i}>
+                  ID: {question.questionID} - Topic: {question.questionTopic} ::
+                  Question : {question.questionsGrid}
+                  Answers : {question.answersGrid}
+                </li>
+              ))}
+            </ul>
+          ) : (
+            <p>No questions available</p>
+          )}
+        </>
+      ) : (
+        <p>Loading modules...</p>
       )}
     </div>
   );
