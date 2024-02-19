2eea5efb5fdda04b7ee237c8f6951920aad5bb62
Dreadlock0926 <105129406+Dreadlock0926@users.noreply.github.com>
2024-02-19 17:35:28 +0530

    Added answer logging

--- frontend/src/pages/TestPages/ExamFinal.jsx
+++ frontend/src/pages/TestPages/ExamFinal.jsx
@@ -10,18 +10,79 @@ const ExamFinalized = () => {
   const examData = sessionStorage.getItem("examData");
 
   if (examData) {
-    console.log(examData);
   } else {
     window.location.href = "/scope";
   }
 
   //might have to use some local storage approach for this
 
+  // getting answers
+
+  let answerValues = [];
+  let correctAnswers = [];
+  let wrongAnswersIndex = [];
+  let wrongQuestions = [];
+
+  const getAnswers = () => {
+    const answers = document.querySelectorAll(".answer-input");
+    answerValues = Array.from(answers).map((answer) => answer.value);
+    console.log("Answers:", answerValues);
+
+    correctAnswers = [];
+    JSON.parse(examData).forEach(question => {
+        question.answersGrid.forEach(answer => {
+            if (answer !== "") {
+                correctAnswers.push(answer);
+            }
+        }) 
+    });
+    console.log(correctAnswers);
+    compareAnswers();
+    addWrongAnswers();
+};
+
+const compareAnswers = () => {
+    wrongAnswersIndex = [];
+    for (let i = 0; i < correctAnswers.length; i++) {
+
+        if (answerValues[i] !== correctAnswers[i]) {
+            wrongAnswersIndex.push(i);
+        }
+        
+    }
+    console.log(wrongAnswersIndex);
+};
+
+const addWrongAnswers = () => {
+
+    let count = -1;
+    wrongQuestions = [];
+
+    JSON.parse(examData).forEach(question => {
+        question.answersGrid.forEach(answer => {
+            if (answer !== "") {
+                count += 1;
+            }
+            if (wrongAnswersIndex.includes(count) && (!wrongQuestions.includes(question.questionID))) {
+
+                wrongQuestions.push(question.questionID);
+
+            }
+        }) 
+    });
+
+    console.log(wrongQuestions);
+
+};
+
+  // end of getting answers
+
   return (
     <div>
       {examData ? (
         <div>
           <h1>Exam</h1>
+          <button onClick={getAnswers}>log answers</button>
           <div>
             {JSON.parse(examData).map((question, index) => {
               return (
