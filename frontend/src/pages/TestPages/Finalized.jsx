1305b7e6d27e2c4ba890ed4b795789c1d9ec5528
Dreadlock0926 <105129406+Dreadlock0926@users.noreply.github.com>
2024-02-25 15:21:55 +0530

    Revert "updating new progress attempt"
    This reverts commit 8756cb1d1126f66b15f91f0edd1600134c312792.

--- frontend/src/pages/TestPages/Finalized.jsx
+++ frontend/src/pages/TestPages/Finalized.jsx
@@ -1,71 +1,79 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable no-unused-vars */
 import Axios from "axios";
-import { useContext, useEffect, useState } from "react";
+import { useContext, useEffect } from "react";
 import { UserContext } from "../../App";
 
+//im just making a small mock idea on how to track  progress overtime!
+
 const Finalized = () => {
-    const { user, setUser } = useContext(UserContext);
-    const [progressOvertime, setProgressOvertime] = useState([]);
 
-    useEffect(() => {
-        previousRecords();
-    }, []);
+    const  {user,setUser} = useContext(UserContext);
+    let outcome = [];
 
-    async function previousRecords() {
-        try {
+   async function previousRecords(){
+        try{
             const response = await Axios.get("http://localhost:8000/progress");
-            if (response.status === 200) {
-                setProgressOvertime(response.data);
-            } else {
-                console.log("No response!");
+            if(response.status===200){
+                outcome = response.data;
+            }else{
+                console.log("no response!");
             }
-        } catch (err) {
+        }catch(err){
             console.error(err);
         }
     }
+   const marks = localStorage.getItem("marks")
+    const progressOvertime = [];
+    const finalOutcome = outcome.filter((x)=>x.marks)
+    progressOvertime.push(finalOutcome);
+    console.log(`The outcome is ${JSON.stringify(finalOutcome)}`);
 
-    useEffect(() => {
-        const marks = localStorage.getItem("marks");
-        if (marks !== null) {
-            const updatedProgress = [...progressOvertime, marks];
-            setProgressOvertime(updatedProgress);
-            UpdateNewProgress(updatedProgress);
-        }
-    }, []);
+    useEffect(()=>{
+        previousRecords();
+    },[])
 
+ 
+      progressOvertime.push(marks)
+
+      
     function OvertimeRecords() {
-        if (progressOvertime.length < 2) {
-            return 0; 
+        for (let i = 0; i < progressOvertime.length; i++) {
+            let todayProgress = progressOvertime[progressOvertime.length - 1] - progressOvertime.slice(0,-1);
+            console.log(`The overtime records are ${todayProgress}`);
+            return todayProgress;
         }
-        const todayProgress = progressOvertime[progressOvertime.length - 1] - progressOvertime[0];
-        console.log(`The overtime records are ${todayProgress}`);
-        return todayProgress;
     }
+    
 
-    async function UpdateNewProgress(updatedProgress) {
-        try {
-            const data = await Axios.post("http://localhost:8000/progress", { progress: updatedProgress });
-            if (data.status === 200) {
+ 
+    async function UpdateNewProgress(){
+        try{
+            const data = await Axios.post("http://localhost:8000/progress",{progress:OvertimeRecords()})
+            if(data.status===200){
                 console.log("Added new records!");
-            } else {
+            }else{
                 console.log("Couldn't add new records!");
             }
-        } catch (err) {
+        }catch(err){
             console.error(err);
         }
     }
 
-    return (
-        <div style={{ textAlign: "center" }}>
-            <h1>Finalized</h1>
-            <p>
-                {OvertimeRecords() !== 0
-                    ? `Congrats you've made ${OvertimeRecords()} progress today!`
-                    : "Seems like there's no new progress today, better luck next time!"}
-            </p>
-        </div>
-    );
-};
+    useEffect(()=>{
+        setTimeout(()=>{
+    UpdateNewProgress();
+        },2000)    
+    },[])
+    
 
-export default Finalized;
+    
+  return (
+    <div style={{textAlign:"center"}}>
+        <h1>Finalized</h1>
+        <p>{`${OvertimeRecords() !== 0  ? `Congrats you've made ${OvertimeRecords()} progress today!` : "Seems like there's no new progress today , better luck next time!"}`}</p>
+        </div>
+  )
+}
+
+export default Finalized
