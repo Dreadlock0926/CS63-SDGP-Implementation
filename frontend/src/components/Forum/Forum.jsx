66e716e588d0ae9a1ec23105ec7c5f5c0bd7196e
Veloxal <dulransamarasinghe4@gmail.com>
2024-02-26 16:05:33 +0530

    minor theme changes

--- frontend/src/components/Forum/Forum.jsx
+++ frontend/src/components/Forum/Forum.jsx
@@ -5,6 +5,7 @@ import { useNavigate } from "react-router-dom";
 import Axios from "axios";
 import { Link } from "react-router-dom";
 import { UserContext } from "../../App";
+import { Button, FormControl, Input, InputLabel, MenuItem, Select, Typography } from '@mui/material';
 import "./Forum.css";
 
 const Forum = () => {
@@ -12,6 +13,7 @@ const Forum = () => {
   const [data, setData] = useState([]);
   const [answer, setAnswer] = useState("");
   const [votes, setVotes] = useState(0);
+  const [down, setDown] = useState(0);
   const navigator = useNavigate();
   const EndPoint = "http://localhost:8000/forum";
   
@@ -33,13 +35,7 @@ const Forum = () => {
 
   const increaseVotes = async (id) => {
     try {
-      
-    
       setLoading(true);
-      // data.filter((x) => {
-      //   totalVotes += x.rating[id];
-      // });
-
       const upvote = await Axios.put(`${EndPoint}/upvotes/${id}`);
       if (upvote.status === 200) {
         setStatus("Upvoted!");
@@ -59,19 +55,13 @@ const Forum = () => {
 
   const downVote = async (id) => {
     try {
-      
-    
       setLoading(true);
-      // data.filter((x) => {
-      //   totalVotes += x.rating[id];
-      // });
-
-      const upvote = await Axios.put(`${EndPoint}/downvotes/${id}`);
-      if (upvote.status === 200) {
+      const downvote = await Axios.put(`${EndPoint}/downvotes/${id}`);
+      if (downvote.status === 200) {
         setStatus("Down Voted!");
         window.location.reload();
       } else {
-        setStatus("Error while upvoting!");
+        setStatus("Error while downvoting!");
       }
       setTimeout(() => {
         navigator("/forum");
@@ -86,8 +76,8 @@ const Forum = () => {
   const AnsweringQuestions = async (id, answer) => {
     try {
       setLoading(true);
-      const r = await Axios.put(`${EndPoint}/${id}`, { answer });
-      if (r.data.status === 200) {
+      const response = await Axios.put(`${EndPoint}/${id}`, { answer });
+      if (response.data.status === 200) {
         setStatus("Answer Posted!");
         setTimeout(() => {
           navigator("/forum");
@@ -114,101 +104,117 @@ const Forum = () => {
     }
   };
 
-  const [down,setDown] = useState(0)
-
-  useEffect(()=>{
+  useEffect(() => {
     console.log(down);
-  },[down])
- 
+  }, [down]);
 
+  const [toggle, setToggle] = useState(false);
+
+  useEffect(() => {
+    if (toggle) {
+      document.body.style.backgroundColor = "black";
+      document.body.style.color = "white";
+    } else {
+      document.body.style.backgroundColor = "white";
+      document.body.style.color = "black";
+    }
+  }, [toggle]);
+  
   return logged ? (
-    <div className="forumContainer">
-      <h1>Welcome back, {user.username || user}!</h1>
+    <div style={{margin:"5%"}}>
+            <Typography variant="h4" style={{textAlign:"center",margin:"5%",fontSize:48}}>Forum</Typography>
+            <button onClick={()=>{setToggle(!toggle)}}>{toggle? "☀️" : "🌙"}</button>
+      <Typography variant="h4">Welcome back, {user.username || user}!</Typography>
       <br />
-      <select onChange={(e)=>setDown(Number(e.target.value))}>
-        <option value={0}>All</option>
-        <option value={1}>Pure Math</option>
-        <option value={2}>Statistics</option>
-      </select>
+      <FormControl>
+        <InputLabel>Select Topic</InputLabel>
+        <Select
+          value={down}
+          onChange={(e) => setDown(Number(e.target.value))}
+        >
+          <MenuItem value={0}>All</MenuItem>
+          <MenuItem value={1}>Pure Math</MenuItem>
+          <MenuItem value={2}>Statistics</MenuItem>
+        </Select>
+      </FormControl>
       {loading ? (
-        <h1>Loading...</h1>
-      ) : data && data.length && down===0 ? (
+        <Typography variant="h5">Loading...</Typography>
+      ) : data && data.length && down === 0 ? (
         data.map((x) => (
           <div key={x._id}>
             <br />
-            <br />
-            <h1>{x.question}</h1>
-            <h2>{x.description}</h2>
-            <h1>{x?.answer ? x.answer : "Be the first to Answer! 🥳"}</h1>
-            <p>{x.by ? `Posted by ${x.by}` : ""}</p>
-            <p>{x.rating ? `Upvoted by ${x.rating}` : <h1>Rated by none!</h1>}</p>
-            <button onClick={(e) => { e.preventDefault(); increaseVotes(x._id) }}>Upvote!</button>
-            <button onClick={(e) => { e.preventDefault(); downVote(x._id) }}>DownVote!</button>
-            <button onClick={(e) => { e.preventDefault(); DeleteComment(x._id) }}>Delete</button>
+            <Typography variant="h4">{x.question}</Typography>
+            <Typography variant="body1">{x.description}</Typography>
+            <Typography variant="h4">{x?.answer ? x.answer : "Be the first to Answer! 🥳"}</Typography>
+            <Typography variant="body2">{x.by ? `Posted by ${x.by}` : ""}</Typography>
+            <Typography variant="body2">{x.rating ? `Upvoted by ${x.rating}` : <Typography variant="h4">Rated by none!</Typography>}</Typography>
+            <Button onClick={(e) => { e.preventDefault(); increaseVotes(x._id) }}>Upvote!</Button>
+            <Button onClick={(e) => { e.preventDefault(); downVote(x._id) }}>DownVote!</Button>
+            <Button onClick={(e) => { e.preventDefault(); DeleteComment(x._id) }}>Delete</Button>
             <br />
             <form className="replyForm" onSubmit={(e) => { e.preventDefault(); AnsweringQuestions(x._id, answer) }}>
-              <input onChange={(e) => { setAnswer(e.target.value) }} placeholder="Answer..." type="text" />
-              <button type="submit">Answer!</button>
+              <Input onChange={(e) => { setAnswer(e.target.value) }} placeholder="Answer..." type="text" />
+              <Button type="submit">Answer!</Button>
             </form>
             <br />
           </div>
         ))
-      ) : data && data.length && down===1 ? (
+      ) : data && data.length && down === 1 ? (
         data.map((x) => (
           x.topic === "Pure Mathematics I" ? (
+           
             <div key={x._id}>
               <br />
-              <br />
-              <h2>{x.topic}</h2>
-              <h2>{x.description}</h2>
-              <h1>{x.question}</h1>
-              <h1>{x?.answer ? x.answer : "Be the first to Answer! 🥳"}</h1>
-              <p>{x.by ? `Posted by ${x.by}` : ""}</p>
-              <p>{x.rating ? `Upvoted by ${x.rating}` : <h1>Rated by none!</h1>}</p>
-              <button onClick={(e) => { e.preventDefault(); increaseVotes(x._id) }}>Upvote!</button>
-              <button onClick={(e) => { e.preventDefault(); downVote(x._id) }}>DownVote!</button>
-              <button onClick={(e) => { e.preventDefault(); DeleteComment(x._id) }}>Delete</button>
+              <Typography variant="h2">{x.topic}</Typography>
+              <Typography variant="body2">{x.description}</Typography>
+              <Typography variant="h4">{x.question}</Typography>
+              <Typography variant="h4">{x?.answer ? x.answer : "Be the first to Answer! 🥳"}</Typography>
+              <Typography variant="body2">{x.by ? `Posted by ${x.by}` : ""}</Typography>
+              <Typography variant="body2">{x.rating ? `Upvoted by ${x.rating}` : <Typography variant="h4">Rated by none!</Typography>}</Typography>
+              <Button onClick={(e) => { e.preventDefault(); increaseVotes(x._id) }}>Upvote!</Button>
+              <Button onClick={(e) => { e.preventDefault(); downVote(x._id) }}>DownVote!</Button>
+              <Button onClick={(e) => { e.preventDefault(); DeleteComment(x._id) }}>Delete</Button>
               <br />
               <form onSubmit={(e) => { e.preventDefault(); AnsweringQuestions(x._id, answer) }}>
-                <input onChange={(e) => { setAnswer(e.target.value) }} placeholder="Answer..." type="text" />
-                <button type="submit">Answer!</button>
+                <Input onChange={(e) => { setAnswer(e.target.value) }} placeholder="Answer..." type="text" />
+                <Button type="submit">Answer!</Button>
               </form>
               <br />
             </div>
           ) : null
         ))
-      ) : data && data.length && down===2 ? (
+      ) : data && data.length && down === 2 ? (
         data.map((x) => (
           x.topic === "Probability And Statistics" ? (
             <div key={x._id}>
               <br />
-              <br />
-              <h2>{x.topic}</h2>
-              <h2>{x.description}</h2>
-              <h1>{x.question}</h1>
-              <h1>{x?.answer ? x.answer : "Be the first to Answer! 🥳"}</h1>
-              <p>{x.by ? `Posted by ${x.by}` : ""}</p>
-              <p>{x.rating ? `Upvoted by ${x.rating}` : <h1>Rated by none!</h1>}</p>
-              <button onClick={(e) => { e.preventDefault(); increaseVotes(x._id) }}>Upvote!</button>
-              <button onClick={(e) => { e.preventDefault(); downVote(x._id) }}>DownVote!</button>
-              <button onClick={(e) => { e.preventDefault(); DeleteComment(x._id) }}>Delete</button>
+              <Typography variant="h2">{x.topic}</Typography>
+              <Typography variant="body2">{x.description}</Typography>
+              <Typography variant="h4">{x.question}</Typography>
+              <Typography variant="h4">{x?.answer ? x.answer : "Be the first to Answer! 🥳"}</Typography>
+              <Typography variant="body2">{x.by ? `Posted by ${x.by}` : ""}</Typography>
+              <Typography variant="body2">{x.rating ? `Upvoted by ${x.rating}` : <Typography variant="h4">Rated by none!</Typography>}</Typography>
+              <Button onClick={(e) => { e.preventDefault(); increaseVotes(x._id) }}>Upvote!</Button>
+              <Button onClick={(e) => { e.preventDefault(); downVote(x._id) }}>DownVote!</Button>
+              <Button onClick={(e) => { e.preventDefault(); DeleteComment(x._id) }}>Delete</Button>
               <br />
               <form onSubmit={(e) => { e.preventDefault(); AnsweringQuestions(x._id, answer) }}>
-                <input onChange={(e) => { setAnswer(e.target.value) }} placeholder="Answer..." type="text" />
-                <button type="submit">Answer!</button>
+                <Input onChange={(e) => { setAnswer(e.target.value) }} placeholder="Answer..." type="text" />
+                <Button type="submit">Answer!</Button>
               </form>
               <br />
             </div>
           ) : null
         ))
       ) : null}
-      <p>{status}</p>
+      <Typography>{status}</Typography>
       <Link to="/addforum">Add question to forum? 🤔</Link>
     </div>
   ) : (
-    <div><h1>Please <Link to="/login">login</Link> to continue to the forum </h1></div>
+    <div>
+      <Typography variant="h1">Please <Link to="/login">login</Link> to continue to the forum </Typography>
+    </div>
   );
 }
-  
 
 export default Forum;
