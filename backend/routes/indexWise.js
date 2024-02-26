cd999b430120b77b0f8ac1ca405674a75ea703de
Veloxal <dulransamarasinghe4@gmail.com>
2024-02-26 19:29:11 +0530

    UGHHHHHHHHHHHH WHY IS THIS NOT WORKINGGGGG

--- backend/routes/indexWise.js
+++ backend/routes/indexWise.js
@@ -4,14 +4,8 @@ const topicsModel = require("../models/topics");
 
 router.post("/", async (req, res) => {
     const theIndex = req.body.theIndex; 
-    if (!theIndex) {
-        return res.status(400).json({ Alert: "No index provided!" }); 
-    }
-
     try {
-        const theTopics = await topicsModel.find({}); 
-        const data = theTopics.find(topic => topic.topics === theIndex);
-        //so the approach above is NOT working
+        const data = await topicsModel.findOne({ topics: theIndex });
         if (!data) {
             return res.status(404).json({ Alert: "No data found!" });
         } else {
@@ -22,6 +16,7 @@ router.post("/", async (req, res) => {
         return res.status(500).json({ Alert: "Internal Server Error" }); 
     }
 });
+
 
 
   module.exports = router;
