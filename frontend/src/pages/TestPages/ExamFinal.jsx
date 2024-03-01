d55296b75e341ad1e55699f4d9cfe513b6253e56
Dreadlock0926 <105129406+Dreadlock0926@users.noreply.github.com>
2024-03-01 12:42:21 +0530

    Revert "welp I tried lol"
    This reverts commit bbfbd4ca8684bfa46a9cec79bfa966528b1e15a7.

--- frontend/src/pages/TestPages/ExamFinal.jsx
+++ frontend/src/pages/TestPages/ExamFinal.jsx
@@ -86,58 +86,59 @@ const addWrongAnswers = () => {
 
 };
 
-const filtered = wrongQuestions.map((question) => question.split("_"));
+const filtered = wrongQuestions.every((question) => question.split("_"));
 const index = [];
 
-filtered.forEach(question => {
-    index.push(question[1]);
-});
-
+index.push(filtered);
+//index contains filtered 
 const outcome = [];
 
 index.forEach((topicKeys) => {
-    switch (topicKeys) {
-        case "rod":
-            outcome.push(0);
-            break;
-        case "pac":
-            outcome.push(1);
-            break;
-        case "p":
-            outcome.push(2);
-            break;
-        case "drv":
-            outcome.push(3);
-            break;
-        case "tnd":
-            outcome.push(4);
-            break;
-        default:
-            break;
-    }
+  switch (topicKeys) {
+    case "rod":
+      outcome.push(0);
+      break;
+    case "pac":
+      outcome.push(1);
+      break;
+    case "p":
+      outcome.push(2);
+      break;
+    case "drv":
+      outcome.push(3);
+      break;
+    case "tnd":
+      outcome.push(4);
+      break;
+    default:
+      break;
+  }
 });
 
+// Now, outcome will contain occurrences of specific keywords, including the initial filtered array
+// If you want to count occurrences of keywords, you might need to refactor your logic
+
 const maxOccurrences = outcome.reduce((maxCount, currentValue) => {
-    const count = outcome.filter((val) => val === currentValue).length;
-    return count > maxCount ? count : maxCount;
+  const count = outcome.filter((val) => val === currentValue).length; //how many times a certain index has repeated
+  return count > maxCount ? count : maxCount; //compare that with 0 , default index (0 representing "rod")
 }, 0);
+//so max count is 0 , if that's the most occuring then that's the index that has repeated the most , else the other index (count)
 
-console.log(`The maxOccuring index is ${maxOccurrences}`);
-
-async function fetchTopic(e) {
-    e.preventDefault();
-    try {
-        const { data } = await Axios.post("http://localhost:8000/index", { theIndex: maxOccurrences });
-        if (data.status == 200) {
-            alert(`The weakest topic is ${data}`);
-        } else {
-            console.log(data);
-        }
-    } catch (err) {
-        console.error(err);
+async function fetchTopic(e){
+  e.preventDefault();
+  try{
+    const {data} = await Axios.post("http://localhost:8000/index",{theIndex:maxOccurrences})
+    if(data.status==200){
+      alert(data)
+    }else{
+      console.log(data);
     }
+  }catch(err){
+    console.error(err);
+  }
 }
 
+console.log(`Max occur is ${JSON.stringify(outcome)}`); // This will give you the maximum occurrence count
 
 
 
