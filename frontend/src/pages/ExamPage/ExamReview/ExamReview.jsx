967e71f717721235b3b21ff305fdbd28f8e86279
Radhul Silva <radhulsilva@gmail.com>
2024-03-18 21:28:46 +0530

    C6SI-11 exam history now passes examID
    it passes through useParams()

--- frontend/src/pages/ExamPage/ExamReview/ExamReview.jsx
+++ frontend/src/pages/ExamPage/ExamReview/ExamReview.jsx
@@ -2,142 +2,147 @@ import "./ExamReview.css";
 import QuestionComponent from "../../../components/QuestionComponent/QuestionComponent";
 import Axios from "axios";
 import { useEffect, useState, useLayoutEffect } from "react";
+import { useParams } from "react-router";
 import "//unpkg.com/mathlive";
 
 function ExamReview() {
+  const { examID } = useParams();
 
-    const [questionIDs, setQuestionIDs] = useState([]);
-    const [questions, setQuestions] = useState([]);
-    const [correctAnswers, setCorrectAnswers] = useState([]);
-    const [userAnswers, setUserAnswers] = useState([]);
+  const [questionIDs, setQuestionIDs] = useState([]);
+  const [questions, setQuestions] = useState([]);
+  const [correctAnswers, setCorrectAnswers] = useState([]);
+  const [userAnswers, setUserAnswers] = useState([]);
 
-    const [examData, setExamData] = useState({});
-    const [loading, setLoading] = useState(true);
+  const [examData, setExamData] = useState({});
+  const [loading, setLoading] = useState(true);
 
-    const addAnswers = () => {
-        const answerFields = document.querySelectorAll("math-field");
-        const subContainers = document.querySelectorAll(".answer-for-sub-question")
+  const addAnswers = () => {
+    const answerFields = document.querySelectorAll("math-field");
+    const subContainers = document.querySelectorAll(".answer-for-sub-question");
 
-        subContainers.forEach(subContainer => {
-            subContainer.style.flexDirection = "column";
-            subContainer.style.alignItems = "flex-start";
-        })
+    subContainers.forEach((subContainer) => {
+      subContainer.style.flexDirection = "column";
+      subContainer.style.alignItems = "flex-start";
+    });
 
-        for (let i = 0; i < answerFields.length; i++) {
+    for (let i = 0; i < answerFields.length; i++) {
+      const p = document.createElement("p");
+      p.style.textAlign = "start";
+      p.style.fontSize = "1.15rem";
+      p.innerText = "Correct Answer:";
+      p.style.fontWeight = "bold";
 
-            const p = document.createElement("p");
-            p.style.textAlign = "start";
-            p.style.fontSize = "1.15rem";
-            p.innerText = "Correct Answer:";
-            p.style.fontWeight = "bold";
+      const mathfield = document.createElement("math-field");
+      mathfield.value = correctAnswers[i];
+      mathfield.style.border = "2px solid green";
+      mathfield.style.marginBlock = "25px";
+      mathfield.style.width = "100%";
+      mathfield.readOnly = "true";
 
-            const mathfield = document.createElement("math-field");
-            mathfield.value = correctAnswers[i];
-            mathfield.style.border = "2px solid green";
-            mathfield.style.marginBlock = "25px";
-            mathfield.style.width = "100%";
-            mathfield.readOnly = "true";
+      const fieldContainer = document.createElement("div");
+      fieldContainer.style.width = "40%";
+      fieldContainer.appendChild(p);
+      fieldContainer.appendChild(mathfield);
 
-            const fieldContainer = document.createElement("div");
-            fieldContainer.style.width = "40%";
-            fieldContainer.appendChild(p);
-            fieldContainer.appendChild(mathfield);
+      answerFields[i].parentNode.insertBefore(
+        fieldContainer,
+        answerFields[i].nextSibling
+      );
+      answerFields[i].value = userAnswers[i];
+      answerFields[i].readOnly = "true";
 
-            answerFields[i].parentNode.insertBefore(fieldContainer, answerFields[i].nextSibling);
-            answerFields[i].value = userAnswers[i];
-            answerFields[i].readOnly = "true";
-            
-            if (userAnswers[i] === correctAnswers[i]) {
-                answerFields[i].style.border = "2px solid green";
-            } else {
-                answerFields[i].style.border = "2px solid red";
-            }
-        }
+      if (userAnswers[i] === correctAnswers[i]) {
+        answerFields[i].style.border = "2px solid green";
+      } else {
+        answerFields[i].style.border = "2px solid red";
+      }
     }
+  };
 
-    const getQuestions = async () => {
-
-        if (questionIDs) {
-
-            let questionsArray = [];
-            let answerArray = [];
-            for (let i = 0; i < questionIDs.length; i++) {
-            
-                try {
-                    const response = await Axios.post('http://localhost:8000/getQuestion', {
-                        "questionID": questionIDs[i]
-                    });
-                    const questionData = response.data;
-                    for (let j = 0; j < questionData.answersGrid.length; j++) {
-                        if (questionData.answersGrid[j] !== "") {
-                            answerArray.push(questionData.answersGrid[j]);
-                        } 
-                    }
-                    questionsArray.push(<QuestionComponent key={i+1} question={questionData} mqNum={i+1}/>);
-                } catch (err) {
-                    console.log(err);
-                }
-        
-            }
-            setQuestions(questionsArray);
-            setCorrectAnswers(answerArray);
-        }
-
-    }
-
-    const getExam = async () => {
-
+  const getQuestions = async () => {
+    if (questionIDs) {
+      let questionsArray = [];
+      let answerArray = [];
+      for (let i = 0; i < questionIDs.length; i++) {
         try {
-            const response = await Axios.post('http://localhost:8000/exam/getExam', {
-                "examRef":"65f6dd481ff32881c7c75cb4"
-            });
-            setExamData(response.data);
+          const response = await Axios.post(
+            "http://localhost:8000/getQuestion",
+            {
+              questionID: questionIDs[i],
+            }
+          );
+          const questionData = response.data;
+          for (let j = 0; j < questionData.answersGrid.length; j++) {
+            if (questionData.answersGrid[j] !== "") {
+              answerArray.push(questionData.answersGrid[j]);
+            }
+          }
+          questionsArray.push(
+            <QuestionComponent
+              key={i + 1}
+              question={questionData}
+              mqNum={i + 1}
+            />
+          );
         } catch (err) {
-            console.log(err);
+          console.log(err);
         }
-
+      }
+      setQuestions(questionsArray);
+      setCorrectAnswers(answerArray);
     }
+  };
 
-    useEffect(() => {
-        getExam();
-    }, [])
+  const getExam = async () => {
+    try {
+      const response = await Axios.post("http://localhost:8000/exam/getExam", {
+        examRef: examID,
+      });
+      setExamData(response.data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
-    useEffect(() => {
-        if (!loading && questions.length === questionIDs.length) {
-        }
-    }, [loading])
+  useEffect(() => {
+    getExam();
+  }, []);
 
-    useEffect(() => {
-        console.log(questions);
-        if (questions.length > 0) {
-            setLoading(false);
-        }
-    }, [questions])
+  useEffect(() => {
+    if (!loading && questions.length === questionIDs.length) {
+    }
+  }, [loading]);
 
-    useEffect(() => {
-        if (examData) {
-            setUserAnswers(examData.userAnswers);
-            setQuestionIDs(examData.examQuestions);
-        }
-    }, [examData])
+  useEffect(() => {
+    console.log(questions);
+    if (questions.length > 0) {
+      setLoading(false);
+    }
+  }, [questions]);
 
-    useEffect(() => {
-        if (questionIDs && questionIDs.length > 0) {
-            getQuestions(); 
-        }
-    }, [questionIDs])
+  useEffect(() => {
+    if (examData) {
+      setUserAnswers(examData.userAnswers);
+      setQuestionIDs(examData.examQuestions);
+    }
+  }, [examData]);
 
-    useEffect(() => {
-        console.log(userAnswers);
-    }, [userAnswers])
+  useEffect(() => {
+    if (questionIDs && questionIDs.length > 0) {
+      getQuestions();
+    }
+  }, [questionIDs]);
 
-    return (<>
-            <div className="question-review-container">
-                {questions}
-            </div>
-            <button onClick={addAnswers}>Add Answers</button>
-        </>
-        )
+  useEffect(() => {
+    console.log(userAnswers);
+  }, [userAnswers]);
+
+  return (
+    <>
+      <div className="question-review-container">{questions}</div>
+      <button onClick={addAnswers}>Add Answers</button>
+    </>
+  );
 }
 
 export default ExamReview;
